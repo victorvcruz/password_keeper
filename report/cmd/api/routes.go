4eb468f4ba454028120955b340352b4a08a20716
victorvcruz <victor.cruz@vocedm.com.br>
2023-07-13 15:23:25 -0300

    feat: add protobuf

--- report/cmd/api/routes.go
+++ report/cmd/api/routes.go
@@ -6,17 +6,21 @@ import (
 	"net"
 	"os"
 	"report.com/cmd/api/handlers"
+	"report.com/internal/auth"
+	"report.com/pkg/middleware"
 	v1 "report.com/pkg/pb"
 )
 
-func New(user *handlers.ReportHandler) error {
+func New(user *handlers.ReportHandler, auth auth.AuthServiceClient) error {
 
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%s", os.Getenv("API_PORT")))
 	if err != nil {
 		return err
 	}
 
-	app := grpc.NewServer()
+	app := grpc.NewServer(
+		grpc.UnaryInterceptor(middleware.NewInterceptor(auth).ServerInterceptor),
+	)
 	v1.RegisterUserServer(app, user)
 
 	err = app.Serve(listener)
---
+++ report/internal/auth/service.go
@@ -0,0 +1,63 @@
+package auth
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log"
+	"os"
+	"report.com/internal/utils/errors"
+	pb2 "report.com/pkg/pb"
+)
+
+type AuthServiceClient interface {
+	AuthUserToken(acessToken string) (int64, error)
+	AuthServiceToken(acessToken string) error
+}
+
+type authService struct {
+	client pb2.AuthClient
+	ctx    context.Context
+}
+
+func NewAuthService() AuthServiceClient {
+
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	conn, err := grpc.Dial(os.Getenv("AUTH_HOST"), opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_client := pb2.NewAuthClient(conn)
+	return &authService{
+		client: _client,
+		ctx:    context.Background(),
+	}
+}
+
+func (a authService) AuthUserToken(acessToken string) (int64, error) {
+	auth, err := a.client.AuthToken(a.ctx, &pb2.AuthTokenRequest{AcessToken: acessToken})
+	if err != nil {
+		return 0, err
+	}
+
+	if !auth.Authorize {
+		return 0, &errors.UnauthorizedTokenError{}
+	}
+
+	return auth.Id, nil
+}
+
+func (a authService) AuthServiceToken(acessToken string) error {
+	auth, err := a.client.AuthApi(a.ctx, &pb2.AuthTokenService{AcessToken: acessToken})
+	if err != nil {
+		return err
+	}
+
+	if !auth.Authorize {
+		return &errors.UnauthorizedTokenError{}
+	}
+
+	return nil
+}
